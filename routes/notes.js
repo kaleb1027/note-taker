4c1c2d3861d09eb210c9b93b513294f3fab1f470
Kaleb Sherrill <kaleb1027@gmail.com>
2022-04-14 16:36:21 -0400

    Deleted excessive comments

--- routes/notes.js
+++ routes/notes.js
@@ -3,12 +3,16 @@ const fs = require("fs");
 const util = require('util');
 const { v4: uuidv4 } = require('uuid');
 
-// Promise version of fs.readFile
+
 const readFromFile = util.promisify(fs.readFile);
+
+
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
+
+  
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
 
-//YOU NEED AT LEAST THIS TO GET A VISIBLE DEPLOYMENT
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -28,9 +26,4 @@ app.get("*", (req,res) =>
 );
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
-//STEPS FOR DEPLOYMENT
-//PUSH EVERYTHING TO GITHUB REPO
-//heroku login in CMD and go to browser to login
-//heroku create in git bash
-//git push heroku main
-//heroku open to open the app in browser
+
