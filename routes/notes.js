2bc2aefdd208b81e8b47d81017f8916b00997f4d
Kaleb Sherrill <kaleb1027@gmail.com>
2022-04-14 16:24:48 -0400

    created api route to index.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,51 @@
-const express = require("express");
-//const app = express();
 const notes = require("express").Router();
 const fs = require("fs");
 const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+  
+
 
 notes.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
 
 module.exports = notes;
